ee205abaa3e5f250ca0d686d4a9e9ada90b12987
John Brunton <john_brunton@hotmail.co.uk>
2013-12-16 21:49:57 +0000

    #7 factored out TabsController

--- app/scripts/directives/tabs.js
+++ app/scripts/directives/tabs.js
@@ -1,27 +1,29 @@
 'use strict';
 
 var tabsModule = angular.module('directives.tabs', []);
+
+var TabsController = function($scope, $element) {
+  var panes = $scope.panes = [];
+
+  $scope.select = function(pane) {
+    angular.forEach(panes, function(pane) {
+      pane.selected = false;
+    });
+    pane.selected = true;
+  }
+
+  this.addPane = function(pane) {
+    if (panes.length == 0) $scope.select(pane);
+    panes.push(pane);
+  }
+};
   
 tabsModule.directive('tabs', function() {
   return {
     restrict: 'E',
     transclude: true,
     scope: {},
-    controller: function($scope, $element) {
-      var panes = $scope.panes = [];
-
-      $scope.select = function(pane) {
-        angular.forEach(panes, function(pane) {
-          pane.selected = false;
-        });
-        pane.selected = true;
-      }
-
-      this.addPane = function(pane) {
-        if (panes.length == 0) $scope.select(pane);
-        panes.push(pane);
-      }
-    },
+    controller: TabsController,
     template:
     '<div>' +
       '<dl class="tabs">' +
